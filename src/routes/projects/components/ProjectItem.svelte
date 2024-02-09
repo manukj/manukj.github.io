4f3e1c74d87b7c1066aef8bb7000e30309b8e81c
Manu <manu1998kj@gmail.com>
2024-02-09 13:18:08 +0000

    Add Card component and update ProjectItem component

--- src/routes/projects/components/ProjectItem.svelte
+++ src/routes/projects/components/ProjectItem.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Card from '$lib/components/Card.svelte';
 	import { ProjectItemModel } from '$lib/data/model/ProjectItemModel';
 	export let project: ProjectItemModel;
 
@@ -9,29 +10,22 @@
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
 <!-- svelte-ignore a11y-click-events-have-key-events -->
+
 <div
-	class="w-96  rounded shadow-slate-600 shadow-lg border-[0.5px]"
+	class="w-96 rounded shadow-slate-600 shadow-lg border-gray-500 border-[1px] cursor-pointer"
 	on:click={() => {
 		moveToUrl(project?.url ?? '');
 	}}
 >
-	<div class="card-body">
-		<h2 class="card-title">{project.name}</h2>
-		<div class="flex flex-row overflow-x-auto">
-			{#each project.techStack as techStack}
-				<img src={techStack.icon} alt="techStack" class="w-5 h-5 mr-1" />
-			{/each}
+	<Card>
+		<div class="card-body">
+			<h2 class="card-title">{project.name}</h2>
+			<div class="flex flex-row overflow-x-auto">
+				{#each project.techStack as techStack}
+					<img src={techStack.icon} alt="techStack" class="w-5 h-5 mr-1" />
+				{/each}
+			</div>
+			<div class="line-clamp-4 text-xs">{project.description}</div>
 		</div>
-		<div class="line-clamp-3 text-xs">{project.description}</div>
-	</div>
-	{#if project.url}
-		<div class="btn-link text-white place-content-center flex pb-4">
-			<button
-				class="card-title"
-				on:click={() => {
-					moveToUrl(project?.url ?? '');
-				}}>More</button
-			>
-		</div>
-	{/if}
+	</Card>
 </div>
